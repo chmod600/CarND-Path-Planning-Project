d497ad5a7f1f03b1fcbfc27c22b259c6c4390f5e
Akshay Khole <akshay@reversehack.in>
2018-08-18 11:49:54 -0700

    Added splines

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -200,6 +201,9 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
+  int lane = 1;
+  double ref_vel = 49.5;
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -236,15 +240,11 @@ int main() {
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
-
           	json msgJson;
-
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
-
-
             // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-            double dist_inc = 0.5;
+            double dist_inc = 0.3;
             for(int i = 0; i < 50; i++) {
               double next_s = car_s + (dist_inc * i + 1);
               double next_d = 6;
@@ -255,14 +255,6 @@ int main() {
                 map_waypoints_x,
                 map_waypoints_y
               );
-
-              //  next_x_vals.push_back(
-              //    car_x + (dist_inc * i) * cos(deg2rad(car_yaw))
-              //  );
-
-              //  next_y_vals.push_back(
-              //    car_y + (dist_inc * i) * sin(deg2rad(car_yaw))
-              //  );
               next_x_vals.push_back(next_xy[0]);
               next_y_vals.push_back(next_xy[1]);
             }
