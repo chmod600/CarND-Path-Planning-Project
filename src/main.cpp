8a3d4f1a5ec2054680847bd47e6089c0f36ccbb0
Akshay Khole <akshay@reversehack.in>
2018-08-18 18:47:11 -0700

    Avoid collison

--- src/main.cpp
+++ src/main.cpp
@@ -202,7 +202,7 @@ int main() {
   }
 
   int lane = 1;
-  double ref_vel = 0;
+  double ref_vel = 49.5;
 
   h.onMessage([&lane, &ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -242,9 +242,30 @@ int main() {
           vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
           int prev_size = previous_path_x.size();
 
-          if(ref_vel < 49.5){
-            ref_vel += .224;
+          if(prev_size > 0) {
+            car_s = end_path_s;
           }
+
+          bool too_close = false;
+          for(int i = 0; i < sensor_fusion.size(); ++i) {
+            float d = sensor_fusion[i][6]; // ith car on the road
+            if(d < (2 + 4 * lane + 2) && d > (2 + 4 * lane - 2)) {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx * vx + vy * vy);
+              double check_car_s = sensor_fusion[i][5];
+              check_car_s += ((double) prev_size * 0.02 * check_speed);
+
+              if((check_car_s > car_s) && ((check_car_s - car_s) < 30)) {
+                ref_vel = 29.; // this is in mph
+              }
+            }
+          }
+
+
+          // if(ref_vel < 49.5){
+          //   ref_vel += .224;
+          // }
 
           // Create a list of widely spaced waypoints
           vector<double> ptsx;
