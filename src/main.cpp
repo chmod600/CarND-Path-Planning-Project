b1baa431b3c1fe881dea9767384e5c0db23a3f16
Akshay Khole <akshay@reversehack.in>
2018-08-19 17:57:05 -0700

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -268,10 +268,9 @@ int main() {
           // Previous path's end s and d values
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
-
+          int prev_size = previous_path_x.size();
           // Sensor Fusion Data, a list of all other cars on the same side of the road.
           vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
-          int prev_size = previous_path_x.size();
 
           if(prev_size > 0) {
             car_s = end_path_s;
@@ -306,8 +305,6 @@ int main() {
                     }
                   }
                 }
-
-
               }
             }
           }
